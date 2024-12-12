f26da669d0175be7323c9e23dc9eb02ff84ea5bb
pzy <2592532554@qq.com>
2024-12-12 14:41:00 +0800

    init commit

---
+++ extract_function_from_d4j.py
@@ -0,0 +1,14 @@
+from pprint import pprint
+from TestParser import TestParser
+
+parser = TestParser("/root/openzeppelin-contracts/libtree-sitter-solidity.so", "solidity")
+buggy_function = dict()
+
+# for project in projects:
+buggy_classes = parser.parse_file("contracts/governance/Governor.sol")
+buggy_methods = dict()
+# print("buggy_classes:")
+pprint(buggy_classes)
+for buggy_class in buggy_classes:
+    for method in buggy_class['methods']:
+        buggy_methods[method["full_signature"]] = method["body"]
